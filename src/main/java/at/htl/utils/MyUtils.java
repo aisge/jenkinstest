5485ba241da575efbe46422fe45cc93a6e8b6ec7
Gerald Aistleitner <g.aistleitner@htl-leonding.ac.at>
2022-10-05 13:35:02 +0200

    fixed method min

--- src/main/java/at/htl/utils/MyUtils.java
+++ src/main/java/at/htl/utils/MyUtils.java
@@ -5,7 +5,7 @@ public class MyUtils {
     public static int getMin(int ... zahlen) {
         int result = zahlen[0];
         for (int i=1; i<zahlen.length; i++) {
-            if (zahlen[i] > result) { 
+            if (zahlen[i] < result) {
                 result = zahlen[i];
             }
         }
